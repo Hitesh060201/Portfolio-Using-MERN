1c9f51eccdd5aaf30256ee27a1c3e7bdff65d9e9
Hitesh060201 <hiteshmishra145@gmail.com>
2024-03-06 16:42:43 +0530

    modification files

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Layout from "./components/Layout/Layout";
 import About from "./pages/About/About";
 import TechStack from "./pages/TechStack/TechStack";
 import Projects from './pages/Projects/Projects';
+import Education from './pages/Education/Education';
 function App() {
   return (
     <>
      <Layout/>
      <div className="container">
      <About/>
+     <Education />
      <TechStack />
      <Projects />
      </div>
